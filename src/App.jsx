0cf56465f67226a0b731b5be002e128569125ee5
yordimul <143403449+yordimul@users.noreply.github.com>
2024-06-04 15:09:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import '../src/css/styles.css'
+import '../src/css/bootstrap.css'
+
+import Header from './componet/Header/Header'
+import Footer from './componet/Footer/Footer'
+import Body from './componet/Body/Body'
+
+function App() {
+  
+  return (
+    <>
+    <Header/>
+    <Body></Body>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componet/Body/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Alert from './Alert'
+import First from './First'
+import Second from './Second'
+import Third from './Third'
+import Fourth from './Fourth'
+import Fifth from './Fifth'
+import Sixth from './Sixth'
+
+function Body() {
+  return (
+    <div>
+        <Alert/>
+    <First/>
+    <Second/>
+    <Third/>
+    <Fourth/>
+    <Fifth/>
+  <Sixth/>
+   </div>
+  )
+}
+
+export default Body
